59fecd75b6eb925401faaac23638b5a8972dd113
Ajayi Davies <ajayidavies7@gmail.com>
2023-01-24 12:16:04 +0100

    first check with new array

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const str of array) {
+    newArray.push(appendString + str);
+  }
+  return newArray;
+}
