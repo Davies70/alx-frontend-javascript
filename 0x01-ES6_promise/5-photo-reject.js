6d085a72ac34cd1f3ac1db53d6e53d20d750b9f8
Ajayi Davies <ajayidavies7@gmail.com>
2023-01-25 12:58:03 +0100

    rejected promise

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,6 @@
+export default function uploadPhoto(fileName) {
+  const promise = new Promise((resolve, reject) => {
+    reject(new Error(`${fileName} cannot be processed`));
+  });
+  return promise;
+}
