2dd3f2854c8568a236e4ad017febb96eecbd3f23
Ajayi Davies <ajayidavies7@gmail.com>
2023-01-25 14:30:20 +0100

    check it

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const array = [];
   return Promise
-    .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((response) => response);
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.forEach(
+      (result) => array.push({ status: result.status, value: result.value }),
+    ));
 }
