c6b8ba55b3167eae030c1634bfe234fa9bf7d539
Ajayi Davies <ajayidavies7@gmail.com>
2023-01-25 11:49:38 +0100

    added return promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => ({ status: 200, body: 'success' }))
+  const obj = { status: 200, body: 'success' };
+  return promise
+    .then(() => obj)
     .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
     });
-  return promise;
 }
