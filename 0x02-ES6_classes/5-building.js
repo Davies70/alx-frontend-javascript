65b551366326130dfad734d53ce20a3f8e7687a3
Ajayi Davies <ajayidavies7@gmail.com>
2023-01-28 02:52:07 +0100

    check again

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,15 @@
-/* eslint-disable class-methods-use-this */
 /* eslint-disable no-underscore-dangle */
+/* eslint-disable class-methods-use-this */
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft === 'number') {
-      this._sqft = sqft;
-    } else {
-      throw new TypeError('sqft must be a number');
-    }
-  }
-
-  evacuationWarningMessage() {
-    const message = 'extending Building must override evacuationWarningMessage';
-    throw new Error(message);
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
