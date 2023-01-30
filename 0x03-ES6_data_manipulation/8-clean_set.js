0d2efedd1c0cd72706c472e341fd6e819b6cdd0a
Ajayi Davies <ajayidavies7@gmail.com>
2023-01-30 16:38:56 +0100

    check 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,11 @@
 export default function clearSet(set, startString) {
     const stringLength = startString.length;
     if (stringLength === 0) return '';
-    const newArray = [];
+    let newArray = [];
     set.forEach((string) => {
+        if (Array.isArray(string)) {
+            newArray = clearSet(set, startString);
+        }
         if (startString === string.slice(0, stringLength)) {
             const slicedString = string.slice(stringLength, string.length);
             newArray.push(slicedString);
