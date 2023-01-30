da42ae38958b4fc1898df3483376ed8b660b1408
Ajayi Davies <ajayidavies7@gmail.com>
2023-01-30 18:00:22 +0100

    map uses set and get method

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 export default function updateUniqueItems(items) {
+  if (items instanceof Map === false) throw new Error('Cannot process');
   for (const key of items.keys()) {
     if (items.get(key) === 1) items.set(key, 100);
   }
