27732d8e8912522b54ec9fd196c68f95e1d05f50
Ajayi Davies <ajayidavies7@gmail.com>
2023-01-30 21:34:00 +0100

    fixed plus plus

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,8 @@
-/* eslint-disable no-plusplus */
 export const weakMap = new WeakMap();
 let counter = 1;
 export function queryAPI(endpoint) {
-  weakMap.set(endpoint, counter++);
+  weakMap.set(endpoint, counter);
+  counter += 1;
   if (counter >= 5) throw new Error('Endpoint load is high');
   return weakMap.get(endpoint);
 }
