1a1f0fedc9f4ec9b90e2a26e794615f636d78042
Ajayi Davies <ajayidavies7@gmail.com>
2023-02-02 13:25:28 +0100

    the interface of the constructor returns an object

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -5,7 +5,7 @@ interface Teacher {
     yearsOfExperience?: number;
     location: string;
     [propName: string]: any;
-};
+}
 
 const teacher3: Teacher = {
     firstName: 'John',
@@ -16,3 +16,50 @@ const teacher3: Teacher = {
   };
 
   console.log(teacher3);
+
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => `${firstName[0]}. ${lastName}`;
+
+console.log(printTeacher("John", "Doe"));
+
+
+interface Student {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): Student;
+  }
+  
+class StudentClass implements Student {
+    firstName : string;
+    lastName : string;
+    constructor (firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+}
