448647c143612b78fda74b3631d7249f1d935c25
Ajayi Davies <ajayidavies7@gmail.com>
2023-02-02 11:18:39 +0100

    task_0 executed

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -20,3 +20,23 @@ const student2: Student = {
 };
 
 const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement("table");
+const tbody = document.createElement("tbody");
+
+studentsList.forEach(student => {
+    const row = document.createElement("tr");
+    row.style.padding = "1px";
+    const td1 = document.createElement("td");
+    const text1 = document.createTextNode(student.firstName);
+    td1.appendChild(text1);
+    const td2 = document.createElement("td");
+    const text2 = document.createTextNode(student.location);
+    td2.appendChild(text2);
+    row.append(td1, td2);
+    tbody.appendChild(row);
+});
+table.appendChild(tbody);
+table.setAttribute('border', '2');
+document.body.append(table);
+
