9370eb3abeaf420825cd29331786d54d3befafb6
Ajayi Davies <ajayidavies7@gmail.com>
2023-02-04 00:21:48 +0100

    final addition

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,13 @@
+/// <reference path="crud.d.ts" />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = {
+    firstName: 'Davies',
+    lastName: 'Ajayi'
+}
+
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = {...row, age: 23 };
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(125);
